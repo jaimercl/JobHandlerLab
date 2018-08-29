82c71a5509dee94b24c0b5d26e3f6f90aab228a2
4010e9 <4010e9@corazon.ece.uprm.edu>
2018-08-29 16:52:09 -0400

    Secon change, salary.

--- src/Position.java
+++ src/Position.java
@@ -67,9 +67,24 @@ public class Position {
 	 */
 	public double getYearlySalary() {
 		
+		double totalYearly;
+		
+		if (jobType == true){
+			
+			totalYearly = hSalary*40*4.5*12;
+					
+					return totalYearly;
+		} else {
+			
+			totalYearly = hSalary*20*4.5*12;
+			
+			return totalYearly;
+			
+		}
+		
 		//Add Code Here [You have to use If/Else to get graded]
 		
-		return 0; //Temporal Return
+		//Temporal Return
 		
 	}
 }
